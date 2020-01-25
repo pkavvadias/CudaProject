d427a329d2c0fd0705aac073615aa424f7423a0b
Panagiotis Kavadias <p.kavvadias@upnet.gr>
2020-01-25 13:35:53 +0200

    function declaration for optimized algorithm

--- Multiplications.cuh
+++ Multiplications.cuh
@@ -1,3 +1,4 @@
 #pragma once
 void cublas_multiplication(const double* A, double* C, const int rows, const int columns);
 __global__ void simple_algorithm(const double* A, double* C, const int rows, const int columns);
+__global__ void optimized_algorithm(const double* A, double* C, const int rows, const int columns);
---
+++ optimizedAlgorithm.cu
@@ -0,0 +1,8 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include "Multiplications.cuh"
+
+__global__ void optimized_algorithm(const double* A, double* C, const int rows, const int columns)
+{
+	
+}
